70b43f5324a35d5b0ab8b40af9eb1c87038a33a0
Christopher Coté <Christopher Cot>
2015-02-09 10:35:32 -0600

    load last 90 days of client fb page insights

--- capuchin/workers/client_insights.py
+++ capuchin/workers/client_insights.py
@@ -1,86 +1,18 @@
 from capuchin import Capuchin
 from capuchin import config
 from capuchin import INFLUX
+from capuchin.insights import INSIGHTS
 from flask_oauth import OAuth
+import urlparse
 import logging
 import time
+import requests
+import datetime
 from slugify import slugify
 
 date_format = "%Y-%m-%dT%H:%M:%S+0000"
 
 class ClientInsights():
-
-    INSIGHTS = [
-        "page_stories",
-        "page_stories_by_story_type",
-        "page_storytellers",
-        "page_storytellers_by_story_type",
-        "page_storytellers_by_age_gender",
-        "page_storytellers_by_city",
-        "page_storytellers_by_country",
-        "page_storytellers_by_locale",
-        "page_impressions",
-        "page_impressions_unique",
-        "page_impressions_paid",
-        "page_impressions_paid_unique",
-        "page_impressions_organic",
-        "page_impressions_organic_unique",
-        "page_impressions_viral",
-        "page_impressions_viral_unique",
-        "page_impressions_by_story_type",
-        "page_impressions_by_story_type_unique",
-        "page_impressions_by_city_unique",
-        "page_impressions_by_country_unique",
-        "page_impressions_by_locale_unique",
-        "page_impressions_by_age_gender_unique",
-        "page_impressions_frequency_distribution",
-        "page_impressions_viral_frequency_distribution",
-        "page_impressions_by_paid_non_paid",
-        "page_impressions_by_paid_non_paid_unique",
-        "page_engaged_users",
-        "page_consumptions",
-        "page_consumptions_unique",
-        "page_consumptions_by_consumption_type",
-        "page_consumptions_by_consumption_type_unique",
-        "page_places_checkin_total",
-        "page_places_checkin_total_unique",
-        "page_places_checkin_mobile",
-        "page_places_checkin_mobile_unique",
-        "page_places_checkins_by_age_gender",
-        "page_places_checkins_by_locale",
-        "page_places_checkins_by_country",
-        "page_negative_feedback",
-        "page_negative_feedback_unique",
-        "page_negative_feedback_by_type",
-        "page_negative_feedback_by_type_unique",
-        "page_positive_feedback_by_type",
-        "page_positive_feedback_by_type_unique",
-        "page_fans_online",
-        "page_fans_online_per_day",
-        "page_fans",
-        "page_fans_locale",
-        "page_fans_city",
-        "page_fans_country",
-        "page_fans_gender_age",
-        "page_fan_adds",
-        "page_fan_adds_unique",
-        "page_fans_by_like_source",
-        "page_fans_by_like_source_unique",
-        "page_fan_removes",
-        "page_fan_removes_unique",
-        "page_fans_by_unlike_source_unique",
-        "page_posts_impressions",
-        "page_posts_impressions_unique",
-        "page_posts_impressions_paid",
-        "page_posts_impressions_paid_unique",
-        "page_posts_impressions_organic",
-        "page_posts_impressions_organic_unique",
-        "page_posts_impressions_viral",
-        "page_posts_impressions_viral_unique",
-        "page_posts_impressions_frequency_distribution",
-        "page_posts_impressions_by_paid_non_paid",
-        "page_posts_impressions_by_paid_non_paid_unique",
-    ]
 
     def __init__(self, client):
         self.oauth = OAuth()
@@ -116,13 +48,33 @@ class ClientInsights():
 
     def get_insights(self):
         id = self.client.facebook_page.id
-        for i in self.INSIGHTS:
+        for i in INSIGHTS:
             res = self.fb_app.get(
                 "/v2.2/{}/insights/{}".format(id, i),
                 data={"period":"day"}
             )
             logging.info(res.data)
             self.write_data(res.data)
+            self.page(res.data)
+
+    def page(self, data):
+        nex = data.get("paging", {}).get("previous")
+        end_date = int(urlparse.parse_qs(urlparse.urlparse(nex).query)['until'][0])
+        end_date = datetime.datetime.fromtimestamp(end_date)
+        stop = end_date - datetime.timedelta(days=90)
+        logging.info(end_date)
+        last = nex
+        while nex:
+            res = requests.get(nex)
+            data = res.json()
+            logging.info(data)
+            self.write_data(data)
+            nex = data.get("paging", {}).get("previous")
+            end_date = int(urlparse.parse_qs(urlparse.urlparse(nex).query)['until'][0])
+            end_date = datetime.datetime.fromtimestamp(end_date)
+            if end_date < stop or not data.get("data"): nex = None
+
+        return True
 
     def write_influx(self, event):
         time, val, typ = event
