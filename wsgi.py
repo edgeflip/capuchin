e7a55f01e97bd40aeb27103bce6b8bf2d5e33a81
Christopher Coté <Christopher Cot>
2015-02-10 14:37:39 -0600

    import env variables

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,10 @@
 def app(env, start_response):
+    import os
     from werkzeug.wsgi import peek_path_info
     from capuchin import Capuchin
     from capuchin import config
     _app = Capuchin()
+    os.environ['SERVER_NAME'] = env['SERVER_NAME']
     if peek_path_info(env) == "healthcheck":
         _app.config['SERVER_NAME'] = None
     else:
