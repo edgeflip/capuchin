40b9e3f0f1f3c3ca3149ba9dfa511432c71f1d2f
Christopher Coté <Christopher Cot>
2015-02-10 14:18:53 -0600

    workaround for healthcheck endpoint

--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,11 @@
-from capuchin import Capuchin
-app = Capuchin()
+def app(env, start_response):
+    from werkzeug.wsgi import peek_path_info
+    from capuchin import Capuchin
+    from capuchin import config
+    _app = Capuchin()
+    if peek_path_info(env) == "healthcheck":
+        _app.config['SERVER_NAME'] = None
+    else:
+        _app.config['SERVER_NAME'] = config.SERVER_NAME
+
+    return _app(env, start_response)
