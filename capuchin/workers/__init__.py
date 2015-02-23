b87e77e75f1daf251461eb68f320d2cacca0724a
Christopher Cote <chris@entropealabs.com>
2015-02-23 16:28:59 -0600

    wrap scheduled tasks in try

--- capuchin/workers/__init__.py
+++ capuchin/workers/__init__.py
@@ -15,22 +15,28 @@ app.config_from_object('capuchin.celeryconfig')
 @app.task
 def get_insights():
     for client in Client.find():
-        last = client.social.facebook.last_sync
-        if not last:
-            last = datetime.datetime.utcnow() - datetime.timedelta(days=90)
-        logging.info("Last Insights for {}: {}".format(client.name, last))
-        i = Insights(client=client, id=client.social.facebook.id, since=last)
-        client.social.facebook.last_sync = datetime.datetime.utcnow()
-        client.save()
+        try:
+            last = client.social.facebook.last_sync
+            if not last:
+                last = datetime.datetime.utcnow() - datetime.timedelta(days=90)
+            logging.info("Last Insights for {}: {}".format(client.name, last))
+            i = Insights(client=client, id=client.social.facebook.id, since=last)
+            client.social.facebook.last_sync = datetime.datetime.utcnow()
+            client.save()
+        except Exception as e:
+            logging.error(e)
 
 @app.task
 def get_feeds():
     for client in Client.find():
-        last = client.last_post
-        logging.info("Last Posts for {}: {}".format(client.name, last))
-        i = ClientPosts(client=client, since=last)
-        client.last_post = datetime.datetime.utcnow()
-        client.save()
+        try:
+            last = client.last_post
+            logging.info("Last Posts for {}: {}".format(client.name, last))
+            i = ClientPosts(client=client, since=last)
+            client.last_post = datetime.datetime.utcnow()
+            client.save()
+        except Exception as e:
+            logging.error(e)
 
 get_insights()
 get_feeds()
