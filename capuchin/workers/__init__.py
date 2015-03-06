44dced5f0a597d8a2a49d5805a568d118c586a10
Christopher Cote <chris@entropealabs.com>
2015-03-06 11:29:11 -0600

    insights fix

--- capuchin/workers/__init__.py
+++ capuchin/workers/__init__.py
@@ -14,7 +14,7 @@ humongolus.settings(logging, db.init_mongodb())
 
 @app.task
 def client_insights(client):
-    if not client is Client: client = Client(id=client)
+    if not isinstance(client, Client): client = Client(id=client)
     last = client.social.facebook.last_sync
     if not last:
         last = datetime.datetime.utcnow() - datetime.timedelta(days=90)
@@ -27,13 +27,13 @@ def client_insights(client):
 def get_insights():
     for client in Client.find():
         try:
-            clint_insights(client)
+            client_insights(client)
         except Exception as e:
             logging.error(e)
 
 @app.task
 def client_feed(client):
-    if not client is Client: client = Client(id=client)
+    if not isinstance(client, Client): client = Client(id=client)
     last = client.last_post
     logging.info("Last Posts for {}: {}".format(client.name, last))
     i = ClientPosts(client=client, since=last)
