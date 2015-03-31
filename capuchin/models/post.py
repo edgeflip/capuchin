2a3352a47b72a15dc470521859d5838202e317e7
Jesse London <jesselondon@gmail.com>
2015-03-31 11:44:40 -0500

    made post view use page id and post fbid encoded in its composite id rather than depend on user's client's facebook configuration name

--- capuchin/models/post.py
+++ capuchin/models/post.py
@@ -2,26 +2,40 @@ from capuchin.models import ESObject
 from capuchin import config
 from flask import url_for
 
+
 class Post(ESObject):
     TYPE = config.POST_RECORD_TYPE
-
-    def url(self):
-        return url_for('engagement.view', id=self.id)
 
     @classmethod
     def filter(cls, client, q, sort):
         q = {
-            "query":{
-                "query_string":{
-                    "default_field":"message",
-                    "query":q
+            "query": {
+                "query_string": {
+                    "default_field": "message",
+                    "query": q
                 }
             },
-            "filter":{
-                "term":{
-                    "client":str(client._id)
+            "filter": {
+                "term": {
+                    "client": str(client._id)
                 }
             },
-            "sort":sort
+            "sort": sort
         }
         return q
+
+    def url(self):
+        return url_for('engagement.view', id=self.id)
+
+    def _idparts(self):
+        return self.id.split('_')
+
+    @property
+    def page_id(self):
+        (page_id, _post_id) = self._idparts()
+        return page_id
+
+    @property
+    def fbid(self):
+        (_page_id, post_id) = self._idparts()
+        return post_id
